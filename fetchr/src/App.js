05ee0814f59dd6bfd569ac56f3441bc08631ff49
Matthew Tucker <matthewt263@gmail.com>
2018-08-20 16:12:55 -0700

    edited routing

--- fetchr/src/App.js
+++ fetchr/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Link, Switch, Route } from 'react-router-dom'
+
+import './App.css';
 
 import Splash from './Components/Splash/Splash.js';
 import SignUp from './Components/SignUp/SignUp.js';
@@ -13,16 +13,20 @@ class App extends Component {
   render() {
     return (
       <div className=".App">
-        <div>
+
+        <div className=".App-navigation">
+
           <h1 className=".App-title">Fetchr</h1>
           <Link to="/">Splash</Link>
           <Link to="/sign-up/">Sign Up</Link>
           <Link to="/sign-in/">Sign In</Link>
           <Link to="/user-page/">My Info.</Link>
           <Link to="/browse/">Find a Friend</Link>
+          
         </div>
 
-        <div>
+        <div className="App-mainContent">
+
           <Switch>
             <Route exact path='/' component={Splash} />
             <Route exact path='/sign-up/' component={SignUp} />
@@ -30,6 +34,7 @@ class App extends Component {
             <Route exact path='/user-page/' component={UserPage} />
             <Route path="/browse/" component={Browse} />
           </Switch>
+
         </div>
 
       </div>
