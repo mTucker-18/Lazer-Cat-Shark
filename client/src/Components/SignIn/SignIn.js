637c117abca65b6458fda02058858da8ae3f2e1f
Matthew Tucker <matthewt263@gmail.com>
2018-08-27 17:19:05 -0700

    finished sign up and sign in pages

--- client/src/Components/SignIn/SignIn.js
+++ client/src/Components/SignIn/SignIn.js
@@ -26,20 +26,24 @@ class SignIn extends Component {
   render() {
     return (
       <div className="SignIn">
-        <h1>Woof, Welcome Back!</h1>
-          <h2>Email: {this.props.email}</h2>
-          <input
+        <div className="InputFields">
+          <h1 className="SignIn--title">woof! welcome back</h1>
+            <h2>email: {this.props.email}
+            <input
               placeholder="Enter your email"
               value={this.props.email}
               onChange={this.onEmailChange}
             />
-            <h2>Password: {this.props.name}</h2>
-          <input
-                placeholder="Enter your password"
-                value={this.props.password}
-                onChange={this.onPasswordChange}
-              />
-        <Button>Sign In</Button>
+            </h2>
+            <h2>password: {this.props.name}
+            <input
+              placeholder="Enter your password"
+              value={this.props.password}
+              onChange={this.onPasswordChange}
+            />
+            </h2>
+          <Button>sign in</Button>
+        </div>
       </div>
     );
   }
