780a41e076d99a4c54b4a5b6941bf2d13ab099df
Maddy Ford <mrhf008@gmail.com>
2018-08-28 21:51:40 -0700

    modified form for redirecting to browse view

--- client/src/Components/SignUp/SignUp.js
+++ client/src/Components/SignUp/SignUp.js
@@ -37,9 +37,6 @@ class SignUp extends Component {
       body: JSON.stringify(data)
     })
     .then(response => response.json())
-    .then(responseData => {
-      console.log("DATA", responseData)
-    });
   }
 
   onNameChange = (ev) => {
@@ -105,80 +102,83 @@ class SignUp extends Component {
       <div className="SignUp">
         <div className="InputFields">
           <h1 className="SignUp--title">join us to find friends for your doggos!</h1>
-          <h2>your name: {this.props.username}
-            <input
-              name="name"
-              placeholder="enter your name"
-              value={this.props.username}
-              onChange={this.onNameChange}
-            />
-          </h2>
-          <h2>your email: {this.props.email}
-            <input
-              name="email"
-              placeholder="enter email"
-              value={this.props.email}
-              onChange={this.onEmailChange}
-            />
-          </h2>
-          <h2>password: {this.props.password}
-            <input
-              name="password"
-              placeholder="create a password"
-              value={this.props.password}
-              onChange={this.onPasswordChange}
-            />
-          </h2>
-          <h2>your address: {this.props.address}
-            <input
-              name="address"
-              placeholder="create a password"
-              value={this.props.address}
-              onChange={this.onAddressChange}
-            />
-          </h2>
-          <h3>(This is not shared)</h3>
-          <h2>how close should dog friends be? {this.props.radius}
-            <input
-              name="radius"
-              placeholder="miles"
-              value={this.props.radius}
-              onChange={this.onRadiusChange}
-            />
-          </h2>
-          <h2>tell us about you and your dog: {this.props.bio}
-            <textarea
-              name="bio"
-              placeholder="Likes, dislikes, etc."
-              value={this.props.bio}
-              onChange={this.onBioChange}
-            />
-          </h2>
-          <h2>dog name: {this.props.dog_name}
-            <input
-              name="dog_name"
-              placeholder="Dog name"
-              value={this.props.dog_name}
-              onChange={this.onDogNameChange}
-            />
-          </h2>
-          <h2>dog size: {this.props.dog_size}
-            <input
-              name="dog_size"
-              placeholder="Small, medium, or large"
-              value={this.props.dog_size}
-              onChange={this.onDogSizeChange}
-            />
-          </h2>
-          <h2>dog energy level: {this.props.dog_energy}
-            <input
-              name="dog_energy"
-              placeholder="Peaceful, super active, etc."
-              value={this.props.dog_energy}
-              onChange={this.onDogEnergyChange}
-            />
-          </h2>
-          <Button onClick={this.onSubmit}>sign up</Button>
+          <form action="/browse">
+            <h2>your name: {this.props.username}
+              <input
+                name="name"
+                placeholder="enter your name"
+                value={this.props.username}
+                onChange={this.onNameChange}
+              />
+            </h2>
+            <h2>your email: {this.props.email}
+              <input
+                name="email"
+                placeholder="enter your email"
+                value={this.props.email}
+                onChange={this.onEmailChange}
+              />
+            </h2>
+            <h2>password: {this.props.password}
+              <input
+                type="password"
+                name="password"
+                placeholder="create a password"
+                value={this.props.password}
+                onChange={this.onPasswordChange}
+              />
+            </h2>
+            <h2>your address: {this.props.address}
+              <input
+                name="address"
+                placeholder="create a password"
+                value={this.props.address}
+                onChange={this.onAddressChange}
+              />
+            </h2>
+            <h3>(only you can see this)</h3>
+            <h2>how close should dog friends be? {this.props.radius}
+              <input
+                name="radius"
+                placeholder="miles"
+                value={this.props.radius}
+                onChange={this.onRadiusChange}
+              />
+            </h2>
+            <h2>tell us about you and your dog: {this.props.bio}
+              <textarea
+                name="bio"
+                placeholder="Likes, dislikes, etc."
+                value={this.props.bio}
+                onChange={this.onBioChange}
+              />
+            </h2>
+            <h2>dog name: {this.props.dog_name}
+              <input
+                name="dog_name"
+                placeholder="Dog name"
+                value={this.props.dog_name}
+                onChange={this.onDogNameChange}
+              />
+            </h2>
+            <h2>dog size: {this.props.dog_size}
+              <input
+                name="dog_size"
+                placeholder="Small, medium, or large"
+                value={this.props.dog_size}
+                onChange={this.onDogSizeChange}
+              />
+            </h2>
+            <h2>dog energy level: {this.props.dog_energy}
+              <input
+                name="dog_energy"
+                placeholder="Peaceful, super active, etc."
+                value={this.props.dog_energy}
+                onChange={this.onDogEnergyChange}
+              />
+            </h2>
+            <Button onClick={this.onSubmit}>sign up</Button>
+          </form>
         </div>
       </div>
     );
