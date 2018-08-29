1d7bbb34dbf9c22eef661959b53827724cc5941f
Maddy Ford <mrhf008@gmail.com>
2018-08-28 21:33:02 -0700

    condensed sign up to one page, working for data inserts

--- client/src/Components/SignUp/SignUp.js
+++ client/src/Components/SignUp/SignUp.js
@@ -20,14 +20,14 @@ class SignUp extends Component {
       name: this.state.name,
       email: this.state.email,
       password: this.state.password,
-      address: '',
-      radius: '',
+      address: this.state.address,
+      radius: this.state.radius,
       likes: [],
       likedBy: [],
-      bio: '',
-      dog_name: '',
-      dog_size: '',
-      dog_energy: '',
+      bio: this.state.bio,
+      dog_name: this.state.dog_name,
+      dog_size: this.state.dog_size,
+      dog_energy: this.state.dog_energy,
     };
     fetch(url, {
       method: "POST",
@@ -38,7 +38,7 @@ class SignUp extends Component {
     })
     .then(response => response.json())
     .then(responseData => {
-      console.log("DATAMEOW", responseData)
+      console.log("DATA", responseData)
     });
   }
 
@@ -47,7 +47,6 @@ class SignUp extends Component {
     this.setState({
       name: value,
     });
-    console.log('getting a new name:', value);
   }
 
   onEmailChange = (ev) => {
@@ -55,7 +54,6 @@ class SignUp extends Component {
     this.setState({
       email: value,
     });
-    console.log('getting a new email:', value);
   }
 
   onPasswordChange = (ev) => {
@@ -63,39 +61,123 @@ class SignUp extends Component {
     this.setState({
       password: value,
     });
-    console.log('getting a new password:', value);
+  }
+
+  onAddressChange = (ev) => {
+    let value = ev.target.value;
+    this.setState({
+      address: value,
+    });
+  }
+  onRadiusChange = (ev) => {
+    let value = ev.target.value;
+    this.setState({
+      radius: value,
+    });
+  }
+  onBioChange = (ev) => {
+    let value = ev.target.value;
+    this.setState({
+      bio: value,
+    });
+  }
+  onDogNameChange = (ev) => {
+    let value = ev.target.value;
+    this.setState({
+      dog_name: value,
+    });
+  }
+  onDogSizeChange = (ev) => {
+    let value = ev.target.value;
+    this.setState({
+      dog_size: value,
+    });
+    console.log(value);
+  }
+  onDogEnergyChange = (ev) => {
+    let value = ev.target.value;
+    this.setState({
+      dog_energy: value,
+    });
   }
   render() {
     return (
       <div className="SignUp">
         <div className="InputFields">
           <h1 className="SignUp--title">join us to find friends for your doggos!</h1>
-          <h2>name: {this.props.username}
-          <input
-            name="name"
-            placeholder="enter your name"
-            value={this.props.username}
-            onChange={this.onNameChange}
-          />
+          <h2>your name: {this.props.username}
+            <input
+              name="name"
+              placeholder="enter your name"
+              value={this.props.username}
+              onChange={this.onNameChange}
+            />
           </h2>
-
-          <h2>email: {this.props.email}
+          <h2>your email: {this.props.email}
             <input
               name="email"
               placeholder="enter email"
               value={this.props.email}
               onChange={this.onEmailChange}
             />
-            </h2>
-
-            <h2>password: {this.props.password}
+          </h2>
+          <h2>password: {this.props.password}
             <input
               name="password"
               placeholder="create a password"
               value={this.props.password}
               onChange={this.onPasswordChange}
             />
-            </h2>
+          </h2>
+          <h2>your address: {this.props.address}
+            <input
+              name="address"
+              placeholder="create a password"
+              value={this.props.address}
+              onChange={this.onAddressChange}
+            />
+          </h2>
+          <h3>(This is not shared)</h3>
+          <h2>how close should dog friends be? {this.props.radius}
+            <input
+              name="radius"
+              placeholder="miles"
+              value={this.props.radius}
+              onChange={this.onRadiusChange}
+            />
+          </h2>
+          <h2>tell us about you and your dog: {this.props.bio}
+            <textarea
+              name="bio"
+              placeholder="Likes, dislikes, etc."
+              value={this.props.bio}
+              onChange={this.onBioChange}
+            />
+          </h2>
+          <h2>dog name: {this.props.dog_name}
+            <input
+              name="dog_name"
+              placeholder="Dog name"
+              value={this.props.dog_name}
+              onChange={this.onDogNameChange}
+            />
+          </h2>
+          <h2>dog size: {this.props.dog_size}
+            <input
+              name="dog_size"
+              placeholder="Small, medium, or large"
+              value={this.props.dog_size}
+              onChange={this.onDogSizeChange}
+            />
+          </h2>
+          <h2>dog energy level: {this.props.dog_energy}
+            <input
+              name="dog_energy"
+              placeholder="Peaceful, super active, etc."
+              value={this.props.dog_energy}
+              onChange={this.onDogEnergyChange}
+            />
+          </h2>
           <Button onClick={this.onSubmit}>sign up</Button>
         </div>
       </div>
